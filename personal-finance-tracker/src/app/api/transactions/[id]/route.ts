f44a0b584a430a8c4f242b6abdb988840ffcbb80
krtatos <kkushagra626@gmail.com>
2025-07-05 15:28:48 +0530

    refactor: Fix parameter type  in PUT and DELETE transaction routes

--- personal-finance-tracker/src/app/api/transactions/[id]/route.ts
+++ personal-finance-tracker/src/app/api/transactions/[id]/route.ts
@@ -2,13 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import TransactionModel from '@/model/TransactionModel';
 import dbConnect from '@/lib/dbConnect';
 
-// PUT /api/transactions/[id]
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
     await dbConnect();
-    const { id } =await context.params;
+    const { id } = await params;
     const body = await req.json();
-
     const updatedTransaction = await TransactionModel.findByIdAndUpdate(id, body, { new: true });
 
     if (!updatedTransaction) {
@@ -21,20 +19,20 @@ export async function PUT(req: NextRequest, context: { params: { id: string } })
   }
 }
 
-// DELETE /api/transactions/[id]
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-  try {
-    await dbConnect();
-    const { id } =await context.params;
 
-    const deleted = await TransactionModel.findByIdAndDelete(id);
-
-    if (!deleted) {
-      return NextResponse.json({ success: false, message: 'Transaction not found' }, { status: 404 });
+export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+      await dbConnect();
+      const { id } = await params;
+  
+      const deleted = await TransactionModel.findByIdAndDelete(id);
+  
+      if (!deleted) {
+        return NextResponse.json({ success: false, message: 'Transaction not found' }, { status: 404 });
+      }
+  
+      return NextResponse.json({ success: true, message: 'Transaction deleted' });
+    } catch (error) {
+      return NextResponse.json({ success: false, message: 'Failed to delete transaction', error }, { status: 500 });
     }
-
-    return NextResponse.json({ success: true, message: 'Transaction deleted' });
-  } catch (error) {
-    return NextResponse.json({ success: false, message: 'Failed to delete transaction', error }, { status: 500 });
   }
-}
